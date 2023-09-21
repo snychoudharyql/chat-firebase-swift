e0d2b5a4bcb4011448b0f9289c759816113a6590
Abhishek Pandey <abhishek.pandey@quokkalabs.com>
2023-09-21 12:29:01 +0530

    [REFACTOR]: Update the code

--- Sources/QLFirebaseChat/Manager/FirebaseAuthManager.swift
+++ Sources/QLFirebaseChat/Manager/FirebaseAuthManager.swift
@@ -9,32 +9,25 @@ import Foundation
 import Firebase
 
 public class FirebaseAuthManager {
+    
     // MARK: - Properties
-
     public static let shared = FirebaseAuthManager()
 
     // MARK: - Initialization
-
-     init() {
-        //FirebaseApp.configure()
-    }
+     init() {}
 
     // MARK: - User Registration
-
     public func registerUser(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if let user = authResult?.user {
                 completion(.success(user))
             } else if let error = error {
                 completion(.failure(error))
-            } else {
-                // Handle unexpected case
             }
         }
     }
 
     // MARK: - User Login
-
     public func loginUser(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
             if let user = authResult?.user {
@@ -48,7 +41,6 @@ public class FirebaseAuthManager {
     }
 
     // MARK: - User Logout
-
     public func logoutUser(completion: @escaping (Result<Void, Error>) -> Void) {
         do {
             try Auth.auth().signOut()
